2669891fb89ef31a091018cde51444af02f86595
Manuel <000326446@adexperu.edu.pe>
2023-08-26 13:37:25 -0500

    footer nuevo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className={'w-full border-t-2 border-dark border-solid dark:border-light'}>
             <Layout className={'py-8 flex justify-center items-center dark:text-light'}>
-                DUDE PUT WEATHER AND LIVE CLOCK DOWN HERE LATER
+                Todos los derechos reservados
             </Layout>
         </footer>
     );
